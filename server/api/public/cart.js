9874b9ff4219e15f1108ea4b6d7c513f774f40a6
Madhu Kumar D S <madhudskumar@live.in>
2017-08-12 13:57:45 +0530

    r:cart logic

--- server/api/public/cart.js
+++ server/api/public/cart.js
@@ -3,19 +3,38 @@ const Moltin = require('../../config/moltin').Moltin,
 
 const _cart = Moltin.Cart;
 
+exports.deleteCart = function (req, res) {
+    // user ends session i.e, app
+    console.log('end session')
+    _cart.Delete().then(() => {
+        console.log('delete')
+        response.res200(res, 'deleted')
+    });
+}
+
+exports.getItems = function (req, res) {
+    _cart.Items()
+    .then((cart) => {
+        response.res200(res, cart)
+    }, (err) => {
+        response.res500(res);
+    })
+}
+
 exports.add = function (req, res) {
     _cart.AddProduct(req.body._id, 1)
-        .then((item) => {
-            response.res200(res, item);
+        .then((cart) => {
+            response.res200(res, cart);
         }, (err) => {
             response.res500(res);
         })
 }
 
 exports.remove = function (req, res) {
-    _cart.RemoveItem(req.body._id, 1)
-        .then((item) => {
-            response.res200(res, item);
+    _cart.RemoveItem(req.body._id)
+        .then((cart) => {
+            console.log(cart)
+            response.res200(res, cart);
         }, (err) => {
             response.res500(res);
         })
--- server/api/public/index.js
+++ server/api/public/index.js
@@ -3,6 +3,8 @@ var public = require('express').Router();
 public
     // ROOT/API/public/
     .get('/games', require('./games').fetchGames)
+    .get('/cart', require('./cart').getItems)
+    .get('/endsession', require('./cart').deleteCart)
     .post('/cart', require('./cart').add)
     .delete('/cart', require('./cart').remove)
 
